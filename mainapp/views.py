4ac53b70161b61ba01d1db525051cdf4fc3e1a1b
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-03-22 17:48:37 +0300

    Добавлены: - пункты категории на главной странице и на остальных при помощи mixins - добавлены страницы этих категорий - переделан BaseView

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Notebook, Smartphone
+from django.views.generic import DetailView, View
+from .models import Notebook, Smartphone, Category
+from .mixins import CategoryDetailMixin
 
 
-def test_view(request):
-    return render(request, 'base.html', {})
+class BaseView(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        return render(request, 'base.html', {'categories': categories})
 
 
 # класс для вывода информации с нескольких моделей.
-class ProductDetailView(DetailView):
+class ProductDetailView(CategoryDetailMixin, DetailView): #Обязательно соблюдать порядок наследования
 
     CT_MODEL_MODEL_CLASS ={
         'notebook': Notebook,
@@ -22,4 +26,13 @@ class ProductDetailView(DetailView):
 
     context_object_name = 'product'
     template_name = 'product_detail.html'
+    slug_url_kwarg = 'slug'
+
+
+class CategoryDetailView(CategoryDetailMixin, DetailView):
+
+    model = Category
+    queryset = Category.objects.all()
+    context_object_name = 'category'
+    template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
