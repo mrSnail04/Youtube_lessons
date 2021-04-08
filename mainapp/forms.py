cedb1f5e7fcec277dc6e4ca6593e986a984e8722
Maksim Bulauski <maks.bulavsky@gmail.com>
2021-04-08 19:48:25 +0300

    Добавлены: - регистрация пользователя - выход из аккаунта

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -46,3 +46,50 @@ class LoginForm(forms.ModelForm):
         model = User
         fields = ['username', 'password']
 
+
+class RegistrationForm(forms.ModelForm):
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    phone = forms.CharField(required=False)
+    address = forms.CharField(required=False)
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.fields['username'].lable = 'Логин'
+        self.fields['password'].lable = 'Пароль'
+        self.fields['confirm_password'].lable = 'Подтвердите пароль'
+        self.fields['phone'].lable = 'Номер телефона'
+        self.fields['first_name'].lable = 'Ваше имя'
+        self.fields['last_name'].lable = 'Ваша фамилия'
+        self.fields['address'].lable = 'Адрес'
+        self.fields['email'].lable = 'Электронная почта'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        domain = email.split('.')[-1]
+        if domain in ['com', 'net']:
+            raise forms.ValidationError(f'Регистрация для домена {domain} невозможна')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(f'Данный почтовый адрес уже зарегистрирован в системе')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'Имя {username} занято')
+        return username
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('Пароли не совпадают')
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'confirm_password', 'first_name', 'last_name', 'address', 'phone', 'email']
+
